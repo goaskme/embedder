cf9cf6df1568c10f61c81478c549aa3999eded31
Bhoomil Shah <bhoomil.shah@incipientinfo.com>
2025-05-16 17:19:55 +0530

    Commented code

--- container_checker.sh
+++ container_checker.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # Tumhara container ka naam yahaan daalo
-CONTAINER_NAME="tumhara-container-naam"
+CONTAINER_NAME="embedder-container"
 
 # Check karo container chal raha hai ya nahi
 if [ "$(docker ps -q -f name=^/${CONTAINER_NAME}$)" ]; then
-    echo "Arey meri jaan, container '$CONTAINER_NAME' already up hai"
+    # echo "Arey meri jaan, container '$CONTAINER_NAME' already up hai"
 else
-    echo "Oh no! Container '$CONTAINER_NAME' down hai"
-    echo "Chalo usko up karte hain abhi ke abhi"
+    # echo "Oh no! Container '$CONTAINER_NAME' down hai"
+    # echo "Chalo usko up karte hain abhi ke abhi"
     docker start $CONTAINER_NAME
 
-    if [ $? -eq 0 ]; then
-        echo "Yayyy! Container '$CONTAINER_NAME' ab up ho gaya hai"
-    else
-        echo "Uff... kuch gadbad ho gayi. Container up nahi hua"
-    fi
+    # if [ $? -eq 0 ]; then
+    #     echo "Yayyy! Container '$CONTAINER_NAME' ab up ho gaya hai"
+    # else
+    #     echo "Uff... kuch gadbad ho gayi. Container up nahi hua"
+    # fi
 fi
 
